858f33a4d2e9babfcb4834dfe41d02b034935525
SamuelsjO <samucagm@rocketmail.com>
2022-04-28 11:34:09 -0300

    Adicionando midleware para checar o token jwt

---
+++ sales-api/src/routes/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import OrderController from '../Controllers/impl/OrderController';
+
+const routes = Router();
+
+routes.get('/api/orders', OrderController.index);
+
+routes.post('api/order');
+
+
+routes.get('/api/status', async (req, res) => {
+                return res.status(200).json({
+                    service:"Sales-API",
+                    status: "UP",
+                    httpStatus: 200
+            
+                })
+           })
+
+export default routes;
